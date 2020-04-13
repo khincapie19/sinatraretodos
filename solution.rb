12bfbf4d8fea54fc40d07e2cb92ece4a6f149104
Katherine <katherinehincapie19@hotmail.com>
2020-04-13 10:19:11 -0400

    Add the name in the URL

--- solution.rb
+++ solution.rb
@@ -1,7 +1,6 @@
 require "sinatra"
 
 get "/makers/:nombre" do
-  @subject = params[:nombre].nil? || params[:nombre].empty? ? "desconocido" : params[:nombre]
-
+  @subject = params[:nombre].nil? || params[:nombre].empty? ? "desconocido" : params[:nombre].capitalize
   erb :index
 end
